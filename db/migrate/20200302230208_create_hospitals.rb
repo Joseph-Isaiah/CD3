0cea0ae98b5713361ce9754f1c9599a8b7a9fe91
Joseph Isaiah <jos.mcnamara@outlook.com>
2020-03-02 17:03:21 -0600

    hospital model added with name and address

---
+++ db/migrate/20200302230208_create_hospitals.rb
@@ -0,0 +1,10 @@
+class CreateHospitals < ActiveRecord::Migration[5.2]
+  def change
+    create_table :hospitals do |t|
+      t.string :name
+      t.text :address
+
+      t.timestamps
+    end
+  end
+end
