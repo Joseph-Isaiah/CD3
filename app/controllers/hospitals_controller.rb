c80904a8346f8535256a4361d063e0e129782cbc
alec quig <alecoralec@gmail.com>
2020-03-09 11:14:23 -0600

    refactor dashboard html page w caio

--- app/controllers/hospitals_controller.rb
+++ app/controllers/hospitals_controller.rb
@@ -1,11 +1,5 @@
 class HospitalsController < ApplicationController
   def dashboard
     @hospital = Hospital.find(params[:id])
-
   end
-
-  def latest_call
-    @latest_call = @hospital.calls.last
-  end
-
 end
