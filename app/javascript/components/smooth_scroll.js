b5ad83e1d125fa160bd0330e148864d8fe1ae0dd
Juan Carlos Banos Mafud <juancarlosbanosmafud@gmail.com>
2020-03-10 10:56:41 -0600

    Finised the logic to separate details

--- app/javascript/components/smooth_scroll.js
+++ app/javascript/components/smooth_scroll.js
@@ -3,12 +3,16 @@ import SmoothScroll from 'smooth-scroll'
 const initSmoothScroll = () => {
   window.smoothScrollTo = new SmoothScroll().animateScroll;
 
-  const nextScrollLinks = document.querySelectorAll('.scroll-next');
-  nextScrollLinks.forEach(link => {
-    const nextScreen = link.closest('.form-screen').nextElementSibling;
-    link.addEventListener('click', _ => {
-      setTimeout(_ => { window.smoothScrollTo(nextScreen) }, 5)
-    });
+  const multiScreenForm = document.querySelector('.multi-screen-form');
+  if (!multiScreenForm) return;
+
+  multiScreenForm.addEventListener('click', event => {
+    const clickedTarget = event.target;
+
+    if(!clickedTarget.closest(".scroll-next")) return;
+
+    const nextScreen = clickedTarget.closest('.form-screen').nextElementSibling;
+    setTimeout(_ => { window.smoothScrollTo(nextScreen) }, 5)
   });
 };
 
